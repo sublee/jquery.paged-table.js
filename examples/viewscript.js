2a3ae4a6929cc1d2fb4d3010479cd4e1a27360d9
Lee, Heungsub <lee@heungsub.net>
2009-07-13 12:14:26 +0900

    Move files.

--- examples/viewscript.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    var script = $('script.code').html().replace(
-        /\/\/ <!\[CDATA\[\s*|\s*\/\/ \]\]>/g, ''
-    );
-    $('<code><pre>' + script.replace(/</g, '&lt;').replace(/>/g, '&gt;')
-                    + '</pre></code>').insertAfter('h1');
-})(jQuery);
-
